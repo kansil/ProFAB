4afc928610643fbc24fc2145a2a3a884cdfbf3b1
Samet <37181660+Sametle06@users.noreply.github.com>
2022-01-09 17:47:20 +0300

    Add files via upload

--- profab/model_evaluate/form_table.py
+++ profab/model_evaluate/form_table.py
@@ -9,6 +9,16 @@ from ..utils import _classif_form_table, _rgr_form_table
 
 def form_table(scores, learning_method = 'classif',path = 'score_path.csv'):
 
+	'''
+	Description:
+		This function saves performance results to a .csv file.
+	Parameters:
+		scores: A dictionary that includes set and their scores
+		learning_method: {'classif,'rgr}, default = 'classif', Type of
+						learnign method classification or regression
+		path: default = 'score_path.csv', A destination where scores are 
+						saved. It must be .csv file.
+	'''
     form_methods = {'classif':_classif_form_table,'rgr':_rgr_form_table}
     form_methods[learning_method](scores = scores, score_path = path)    
     
