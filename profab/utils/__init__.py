e045aefbca7f8c6a3b34b9724e999c7d3e296de3
Sametle06 <odtulugenc@outlook.com>
2022-01-12 18:30:54 +0300

    corrections

---
+++ profab/utils/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 12 17:58:44 2022
+
+@author: Sameitos
+"""
+
+from .imp_split_form import *
+from .feature_extraction_module import feature_extracter
+from .feature_extraction_module.utils import bcolors
