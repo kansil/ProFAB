93c87e30378c244d2c65b50e3e3f92fde8e193e6
Samet <37181660+Sametle06@users.noreply.github.com>
2022-01-12 20:24:15 +0300

    Add files via upload

--- profab/utils/__init__.py
+++ profab/utils/__init__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan 12 17:58:44 2022
+Created on Wed Jan 12 19:51:59 2022
 
 @author: Sameitos
 """
 
 from .imp_split_form import *
+from .imp_split_form import _rgr_data_import, _classif_data_import
 from .feature_extraction_module import feature_extracter
 from .feature_extraction_module.utils import bcolors
