571b691df2c5e98df0bfc4d6335f3ecd245314fd
Samet <37181660+Sametle06@users.noreply.github.com>
2022-01-16 01:10:38 +0300

    chage path

--- profab/utils/feature_extraction_module/download_pssms.sh
+++ profab/utils/feature_extraction_module/download_pssms.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-if [ -f profab/feature_extraction_module/swissprot_pssms.zip ]; then
+if [ -f profab/utils/feature_extraction_module/swissprot_pssms.zip ]; then
 	echo "Aldready downloaded!"
 else
 	echo "Downloading..."
 	if [ -x "$(which wget)" ] ; then
-	    wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1AeDvRgxMIViUz0hKz08UazisseTkvcv9' -O profab/feature_extraction_module/swissprot_pssms.zip
+	    wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1AeDvRgxMIViUz0hKz08UazisseTkvcv9' -O profab/utils/feature_extraction_module/swissprot_pssms.zip
 	    echo "Download completed!"
 	    
 	elif [ -x "$(which curl)" ] ; then
-	    curl 'https://docs.google.com/uc?export=download&id=1AeDvRgxMIViUz0hKz08UazisseTkvcv9' -O profab/feature_extraction_module/swissprot_pssms.zip
+	    curl 'https://docs.google.com/uc?export=download&id=1AeDvRgxMIViUz0hKz08UazisseTkvcv9' -O profab/utils/feature_extraction_module/swissprot_pssms.zip
 	    echo "Download completed!"
 	else 
 	    echo "***Please install wget or curl***"
