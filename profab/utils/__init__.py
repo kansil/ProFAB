4f43641ee571e6793077e7e213848336e3ab24a7
Samet <37181660+Sametle06@users.noreply.github.com>
2022-02-17 15:03:55 +0300

    Update __init__.py

--- profab/utils/__init__.py
+++ profab/utils/__init__.py
@@ -6,6 +6,7 @@ Created on Wed Jan 12 19:51:59 2022
 """
 
 from .imp_split_form import *
+from .imp_split_form import _classif_form_table,_rgr_form_table
 from .imp_split_form import _rgr_data_import, _classif_data_import
 from .feature_extraction_module import feature_extracter
 from .feature_extraction_module.utils import bcolors
