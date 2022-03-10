40173beb0b2048eeb35b1c3b58c380cd56267ca7
Samet <37181660+Sametle06@users.noreply.github.com>
2022-03-10 22:37:40 +0300

    Add files via upload

--- profab/utils/imp_split_form.py
+++ profab/utils/imp_split_form.py
@@ -8,6 +8,7 @@ Created on Tue Mar  9 00:30:06 2021
 import io
 import os
 import re
+import sys
 import random
 import requests
 import numpy as np
@@ -17,14 +18,22 @@ from zipfile import ZipFile
 
 def download_data(server_path,save_path):
     
-    response = requests.get(server_path, stream = True)
-    total_byte= int(response.headers.get('content-length', 0)) 
-    progress_bar = tqdm(total=total_byte, unit='iB', unit_scale=True)
-    with open(save_path, 'wb') as file:
-        for data in response.iter_content(chunk_size= 8*1024):
-            progress_bar.update(len(data))
-            file.write(data)
-    progress_bar.close()
+    headers=requests.head(server_path).headers
+    #print(headers)
+    downloadable = 'Content-Length' in headers.keys()
+    if downloadable:
+        response = requests.get(server_path, stream = True)
+        
+        total_byte= int(response.headers.get('content-length', 0)) 
+        progress_bar = tqdm(total=total_byte, unit='iB', unit_scale=True)
+        with open(save_path, 'wb') as file:
+            for data in response.iter_content(chunk_size= 8*1024):
+                progress_bar.update(len(data))
+                file.write(data)
+        progress_bar.close()
+    else:
+        print("No given dataset is available in server.")
+        sys.exit(1)
 
 def separator(X,y,ratio):
     
