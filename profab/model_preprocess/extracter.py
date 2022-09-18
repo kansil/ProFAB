ff92bcdd87085af9e31cdf8ac758baa827d9ed7d
Samet <37181660+Sametle06@users.noreply.github.com>
2022-09-18 11:35:24 +0300

    Add files via upload

--- profab/model_preprocess/extracter.py
+++ profab/model_preprocess/extracter.py
@@ -14,7 +14,10 @@ import category_encoders as ce
 def extract_protein_feature(protein_feature,
                           place_protein_id,
                           input_folder,
-                          fasta_file_name
+                          fasta_file_name,
+                          take_avg = False,
+                          max_len = -1
+                          
                           ):
     '''
      Parameters:
@@ -24,9 +27,7 @@ def extract_protein_feature(protein_feature,
                           s_fpssm, dpc_pssm, k_separated_bigrams_pssm, eedp, tpc, edp, rpssm,
                           pse_pssm, dp_pssm, pssm_ac, pssm_cc, aadp_pssm, aatp, medp , or all_POSSUM
 
-                          all_POSSUM: it extracts the features of all (21) POSSUM protein descriptors
-
-                          or
+                          all_POSSUM: it extracts the features of all (21) POSSUM protein descriptors,
 
                           one of the 18 protein descriptors in iFeature.
 
@@ -34,14 +35,23 @@ def extract_protein_feature(protein_feature,
                           NMBroto, CTDC, CTDD, CTDT, CTriad, KSCTriad, SOCNumber, QSOrder, or all_iFeature
 
                           all_iFeature: it extracts the features of all (18) iFeature protein descriptors
+                          
+                          or
+                          
+                          one of BERT, T5XL transformer model.
+                          
 
          place_protein_id: {int}, (default = 1): It indicates the place of protein id in fasta header.
                            e.g. fasta header: >sp|O27002|....|....|...., seperate the header wrt. '|' then >sp is
                            in the zeroth position, protein id in the first(1) position.
 
-        input_folder: {string}, (default = 'input_folder'}: it is the path to the folder that contains the fasta file.
+         input_folder: {string}, (default = 'input_folder'}: it is the path to the folder that contains the fasta file.
 
-        fasta_file_name: {string}, (default ='sample'): it is the name of the fasta file exclude the '.fasta' extension.
+         fasta_file_name: {string}, (default ='sample'): it is the name of the fasta file exclude the '.fasta' extension.
+        
+         take_avg: {bool}, (default = False), if True, average of vectors will be returned
+        
+         max_len: {int}, (default = -1), Max sequence lenght to embed
     '''
 
     feat_ext = feature_extracter(protein_feature,
@@ -57,34 +67,44 @@ def extract_protein_feature(protein_feature,
 
         return feat_ext.extract_iFeature_feature()
 
+    elif protein_feature in ['T5XL','BERT']:
+        
+        return extract_deep_feature(fasta_file_name, input_folder, place_protein_id, protein_feature)
+
     else:
-
-        print(f"{bcolors.FAIL}Protein Feature extraction method is not in either POSSUM or iFeature{bcolors.ENDC}")
-
+        print(f"{bcolors.FAIL}Protein Feature extraction method is not in POSSUM, iFeature{bcolors.ENDC} or ProtTrans")
 
 
-def extract_deep_feature(seq_data, pretrained_model= 't5_xl',take_avg = False,max_len = -1):
+
+def extract_deep_feature(fasta_file, input_folder, place_protein_id, protein_feature,take_avg = False,max_len = -1):
     '''
     Description:
         This function is to transform protein sequences into continuous data
         by using RostLab pretrained models with "transformers" Python package
     Parameters:
-        seq_data: {np.array, list}, protein sequence data. 
-        pretrained_model: {str}, {'prot_t5_xl_uniref50','prot_bert'}, 
-            default= "prot_t5_xl_uniref50", pretrained model that RostLab
-            provides to "transformers" package
-        take_avg: {bool}, default = False, if True, average of vectors will be returned
-        max_len: {int}, default = -1, Max sequence lenght to embed
+        fasta_file: {str}, fasta file of protein sequence data. 
+            
+        place_protein_id: {int}, (default = 1): It indicates the place of protein id in fasta header.
+                           e.g. fasta header: >sp|O27002|....|....|...., seperate the header wrt. '|' then >sp is
+                           in the zeroth position, protein id in the first(1) position.
+
+        input_folder: {string}, (default = 'input_folder'}: it is the path to the folder that contains the fasta file.
+
+        fasta_file_name: {string}, (default ='sample'): it is the name of the fasta file exclude the '.fasta' extension.
+        
+        take_avg: {bool}, (default = False), if True, average of vectors will be returned
+        
+        max_len: {int}, (default = -1), Max sequence lenght to embed
+        
+        protein_feature: {str}, {'BERT','T5XL'}, transformer model.
     Return:
         embedded_data: {np.array}, transformed continous data.
     
     '''
-
-
-    if pretrained_model == 't5_xl':
-        return t5_features(seq_data,take_avg,max_len) 
-    elif pretrained_model == 'bert':
-        return bert_features(seq_data,take_avg,max_len)
+    if protein_feature == 'T5XL':
+        return t5_features(fasta_file,input_folder, place_protein_id ,take_avg,max_len)
+    elif protein_feature == 'BERT':
+        return bert_features(fasta_file,input_folder, place_protein_id ,take_avg,max_len)
 
 def onehotencoder():
     pass
